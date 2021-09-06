33e62073f9044273823e73c55f72fe3541d963ba
tkdlxm457 <tkdlxm457@naver.com>
2021-09-06 23:34:44 +0900

    add route update and delete

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 const mysql = require('mysql');
+
+
 const connection = mysql.createConnection(
   {
     host: 'localhost',
@@ -9,10 +13,7 @@ const connection = mysql.createConnection(
   }
 );
 
-const cors = require('cors');
 const domains = ['http://localhost:4200'];
-var bodyParser = require('body-parser')
-
 const corsOptions = {
   origin: function (origin, callback) {
     const isTrue = domains.indexOf(origin) !== -1;
@@ -26,10 +27,8 @@ const corsOptions = {
 const app = express();
 
 app.use(cors(corsOptions));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // configuration =========================
@@ -41,16 +40,38 @@ app.get('/', (req, res) => {
 
 app.get('/board', (req, res) => {
   connection.query('SELECT * from topic', (error, rows) => {
-    if (error) throw error;
+    if (error) {
+      throw error;
+    } 
+
     console.log('User info is: ', rows);
     res.send(rows);
   });
+
 });
 
 
 app.post('/board', (req, res) => {
   
   connection.query(`INSERT INTO topic (title, description, created)  values (?, ?, now());`, [req.body.title, req.body.desc], (error, rows) => {
+    if (error) throw error;
+    res.status(200).send('성공');
+  });
+});
+
+app.put('/board', (req, res) => {
+  
+  connection.query(`UPDATE topic SET title="?", description="?" WHERE id=?;`, [req.body.title, req.body.desc, req.body.id], (error, rows) => {
+    if (error) throw error;
+    res.status(200).send('성공');
+  });
+});
+
+app.delete('/board/:id', (req, res) => {
+
+let sql = `DELETE FROM topic WHERE id=${Number.parseInt(req.params.id)}`;
+
+  connection.query(sql, [], (error, rows) => {
     if (error) throw error;
     res.status(200).send('성공');
   });
